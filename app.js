619ac1d005182a561073bbcba7e04f186f6da839
laure claret <laure.a.claret@gmail.com>
2023-04-24 19:32:29 +1000

    feat(app): update and delete requests

--- app.js
+++ app.js
@@ -44,6 +44,47 @@ app.get("/envelopes/:id", (req, res) => {
   res.send(envelope);
 });
 
+// update a specific envelope
+app.patch("/envelopes/:id", (req, res) => {
+  const { id } = req.params;
+  const envelope = envelopes.find((env) => env.id === Number(id));
+  if (!envelope) {
+    res.status(404).send(`Envelope ${id} not found.`);
+    return;
+  }
+  const { title, budget, value } = req.body;
+  if (title) {
+    envelope.title = title;
+  }
+  if (budget) {
+    envelope.budget = budget;
+  }
+  if (value) {
+    if (envelope.budget < value) {
+      res
+        .status(400)
+        .send(
+          `Cannot extract more than the budget. Current budget: ${envelope.budget}`
+        );
+      return;
+    }
+    envelope.budget -= value;
+  }
+  res.send(`Envelope ${id} updated.`);
+});
+
+// delete a specific envelope
+app.delete("/envelopes/:id", (req, res) => {
+  const { id } = req.params;
+  const index = envelopes.findIndex((env) => env.id === Number(id));
+  if (index === -1) {
+    res.status(404).send(`Envelope ${id} not found.`);
+    return;
+  }
+  envelopes.splice(index, 1);
+  res.send(`Envelope ${id} deleted.`);
+});
+
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
